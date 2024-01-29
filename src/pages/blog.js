036d1635e37952b7183e242052585354aebfeaf7
Saaral_Note <khosoo.fsdev@gmail.com>
2024-01-29 11:46:19 +0800

    asd

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import { Source_Serif_4 } from "next/font/google";
+import React from "react";
+
+export default function Blog(props) {
+  return <div>{props.data.title}</div>;
+}
+
+export const getStaticProps = async (context) => {
+  const result = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await result.json();
+  console.log(data);
+  return {
+    props: { data: data },
+  };
+};
