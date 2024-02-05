48c50b6bea348de9a9d189d3ec3be87777991f31
khosoo-fullstackdev <khosoo.fsdev@gmail.com>
2024-02-06 07:40:38 +0800

    wEz

--- src/pages/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-export function LoadMore() {
-  return (
-    <div className="flex px-[20px] py-[12px] justify-center items-center gap-3 rounded-[6px] bg-[#105, 106, 117, 0.30] border mb-[100px]">
-      <button className="text-[#696A75] font-sans text-[16px] not-italic font-medium leading-6">
-        Load More
-      </button>
-    </div>
-  );
-}
